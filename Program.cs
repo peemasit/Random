3a3d05362ba5fd280d3e1139d17e1823225b1d5c
peemasit <peemasit@gmail.com>
2019-03-25 13:14:02 +0700

    Initial commit

---
+++ Program.cs
@@ -0,0 +1,27 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Random1 {
+    class Program {
+        static void Main(string[] args) {
+
+            Console.WriteLine("Please enter your question!");
+            Console.ReadLine();
+            Random yesNoMaybe = new Random();
+            int answerNum;
+            answerNum = yesNoMaybe.Next(1, 4);
+            if (answerNum == 1) {
+                Console.WriteLine("Yes");
+            } else if (answerNum == 2) {
+                Console.WriteLine("Maybe");
+            } else {
+                Console.WriteLine("No");
+            }
+            
+            Console.ReadKey();
+        }
+    }
+}
